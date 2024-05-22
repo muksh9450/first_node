1911054dda9bf193e5069b244dc9184c2532e6e2
mukesh9450 <mukeshkajala@gmail.com>
2024-05-22 08:13:55 +0530

    first changes by other setup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
-fs.mkdir('json',(err)=>{console.log('folder created')})
+// fs.mkdir('json',(err)=>{console.log('folder created')})
 
-fs.writeFile('./json/index.js','sdfs',(err,data)=>{
-    console.log(data)
-})
+// fs.writeFile('./json/index.js','sdfs',(err,data)=>{
+//     console.log(data)
+// })
 // console.log('main index file')
+console.log('mkk')
